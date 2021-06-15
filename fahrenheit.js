af93cb4747005ff1518ffa42ca1d6886c7db7d69
ChrisDMeadows <53023797+ChrisDMeadows@users.noreply.github.com>
2021-06-15 12:24:20 -0500

    Add files via upload

---
+++ fahrenheit.js
@@ -0,0 +1,13 @@
+// kelvin today is 293
+const kelvin = 0;
+
+// converting to celcius
+const celsius = kelvin - 273;
+
+// converting to Fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+
+//rounding down
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit. `);
